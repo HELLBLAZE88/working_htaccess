34fe7255469766e8c036ee853b17e4a5c6fb589b
HELLBLAZE88 <62383651+HELLBLAZE88@users.noreply.github.com>
2021-01-15 18:11:21 +0700

    Add files via upload

---
+++ index3.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>задание 1</title>
+</head>
+<body>
+	<?php 
+	echo "всем привет в чатике";
+	?>
+</body>
+</html>
---
+++ index7.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>задание 7</title>
+</head>
+<body>
+	<?php
+	echo "никто не купил курсы по тёмной магии....";
+	?>
+</body>
+</html>
